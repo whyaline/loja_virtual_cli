d7007959fd5cb10da546618cfc35693f6147b940
Aline <93549092+whyaline@users.noreply.github.com>
2026-01-03 18:07:15 -0300

    Update pagamento.py - esqueleto

---
+++ src/loja_virtual_cli/pagamento.py
@@ -0,0 +1,11 @@
+class Pagamento:
+    def __init__(self, data, forma, valor):
+        self.data = data
+        self.forma = forma
+        self.valor = valor
+
+    def validar(self):
+        pass
+
+    def confirmar(self):
+        pass

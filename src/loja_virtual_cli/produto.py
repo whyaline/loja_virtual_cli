3b33e42df51c9bd769665baf5e8fc4ced845e0a7
Aline <93549092+whyaline@users.noreply.github.com>
2026-01-06 23:03:02 -0300

    Update produto.py
    add properties e setters nos atributos, terceirização de validações, criação de métodos especiais e determinação da classe abstrata Produto

--- src/loja_virtual_cli/produto.py
+++ src/loja_virtual_cli/produto.py
@@ -1,16 +1,90 @@
-class Produto:
+from abc import ABC, abstractmethod
+
+class Produto (ABC):
     def __init__(self, sku, nome, categoria, preco, estoque, ativo):
-        self.sku = sku #unico
-        self.nome = nome #validar - não vazio
-        self.categoria = categoria #validar - não vazia
-        self.preco = preco #validar - > 0
-        self.estoque = estoque #validar -  >=0
-        self.ativo = ativo
+        self.sku = sku #unico -> validar em repositório
+        self.nome = nome 
+        self.categoria = categoria 
+        self.preco = preco 
+        self.estoque = estoque 
+        self.ativo = ativo 
+
+    @property
+    def sku(self):
+        return self.__sku #implementar automático no repositório
+
+    @sku.setter
+    def sku(self, sku):
+        validar_numero(sku, "SKU", tipo=int, permitir_zero=False)
+        self.__sku = sku
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        validar_string(nome, "Nome")
+        self.__nome = nome
+
+    @property
+    def categoria(self):
+        return self.__categoria
+
+    @categoria.setter
+    def categoria(self, categoria):
+        validar_string(categoria, "Categoria")
+        self.__categoria = categoria
+
+    @property
+    def preco(self):
+        return self.__preco
+
+    @preco.setter
+    def preco(self, preco):
+        validar_numero(preco, "Preço", tipo=float)
+        self.__preco = preco
+
+    @property
+    def estoque(self):
+        return self.__estoque
+
+    @estoque.setter
+    def estoque(self, estoque):
+        validar_numero(estoque, "Estoque", tipo=int, permitir_zero=True)
+        self.__estoque = estoque
+
+    @property
+    def ativo(self):
+        return self.__ativo
+
+    @ativo.setter
+    def ativo(self, ativo):
+        if not isinstance(ativo, bool):
+            raise ValueError("Ativo deve ser um valor booleano")
+        self.__ativo = ativo
+
+
+    #######
+    def tem_estoque(self):
+        return self.estoque > 0
 
     def adicionar_estoque(self, qtde):
+        if not self.ativo:
+            raise ValueError("Produto inativo!")
+
+        validar_numero(qtde, "Quantidade", tipo=int, permitir_zero=False)
         self.estoque += qtde
 
     def remover_estoque(self, qtde):
+        if not self.ativo:
+            raise ValueError("Produto inativo!")
+
+        validar_numero(qtde, "Quantidade", tipo=int, permitir_zero=False)
+
+        if qtde > self.estoque:
+            raise ValueError("Quantidade em estoque insuficiente!")
+
         self.estoque -= qtde
 
     def ativar(self):
@@ -19,44 +93,59 @@ class Produto:
     def desativar(self):
         self.ativo = False
 
-    #update
-    def alterar_nome(self, nome):
-        self.nome = nome
-
-    def alterar_preco(self, preco):
-        self.preco = preco
-
-    def alterar_categoria(self, categoria):
-        self.categoria = categoria
+    @abstractmethod
+    def tem_frete(self):
+        pass
 
     #métodos especiais
     def __str__(self):
-        return f'{self.sku}, {self.nome}, {self.preco}'
+        return f'SKU: {self.sku}, Nome: {self.nome}, Categoria: {self.categoria}, Preço: {self.preco}, Estoque: {self.estoque}, Ativo: {self.ativo}'
 
     def __repr__(self):
-        pass
+        return f'Produto(sku={self.sku!r}, nome={self.nome!r}, categoria={self.categoria!r}, preco={self.preco!r}, estoque={self.estoque!r},  ativo={self.ativo!r})'
 
-    def __eq__(self): #por sku
-        pass
+    def __eq__(self, outro): #por sku
+        if not isinstance(outro, Produto):
+            return NotImplemented
 
-    def __lt__(self): #ordenar por preço ou nome
-        pass 
+        return self.sku == outro.sku
+
+    def __lt__(self, outro): #ordenar por preço ou nome
+        if not isinstance(outro, Produto):
+            return NotImplemented
+
+        return self.preco < outro.preco
+
 
 class ProdutoFisico(Produto):
     def __init__(self, sku, nome, categoria, preco, estoque, ativo, peso):
         super().__init__(sku, nome, categoria, preco, estoque, ativo)
         self.peso = peso #validar - > 0
 
+    @property
+    def peso(self):
+        return self.__peso
+
+    @peso.setter
+    def peso(self, peso):
+        validar_numero(peso, "Peso", tipo=float, permitir_zero=False)
+
+        self.__peso = peso
+
     def tem_frete(self):
        return True
+
+    def __str__(self):
+        return f"{super().__str__()}, Peso: {self.peso}"
 
     #persistência
     def to_dict(self): #incluir tipo físico
         pass
 
+    @classmethod
     def from_dict(cls, dict):
         pass
-        
+
 
 class ProdutoDigital(Produto):
     def tem_frete(self):
@@ -66,9 +155,7 @@ class ProdutoDigital(Produto):
     def to_dict(self): #incluir tipo digital
         pass
 
+    @classmethod
     def from_dict(cls, dict):
         pass
-
-
-
 

c7b693f3a9d0ef71a0344d054a58e2777e9885cd
Aline <93549092+whyaline@users.noreply.github.com>
2026-01-12 11:53:03 +0000

    refactor(item_carrinho.py): implementa item_carrinho

--- src/modelos/item_carrinho.py
+++ src/modelos/item_carrinho.py
@@ -1,12 +1,30 @@
-class itemCarrinho:
-    def __init__(self, produto: Produto, qtde):
+from src.utils.validacoes import validar_numero
+from src.modelos.produto import Produto
+
+class ItemCarrinho:
+    def __init__(self, produto: Produto, qtde: int):
+        validar_numero(qtde, "Quantidade", tipo=int, permitir_zero=False)
+
+        if qtde > produto.estoque:
+            raise ValueError(f"Quantidade solicitada ({qtde}) é maior que o estoque disponível ({produto.estoque})")
+
         self.produto = produto
         self.qtde = qtde
 
-    def adicionar_qtde(self, qtde):
-        self.qtde += qtde #validar se qtde > estoque
+    def adicionar_qtde(self, qtde): #validar se qtde > estoque
+        validar_numero(qtde, "Quantidade", tipo=int, permitir_zero=False)
+
+        if self.qtde + qtde > self.produto.estoque:
+            raise ValueError(f"Quantidade solicitada ({qtde}) é maior que o estoque disponível ({self.produto.estoque})")
+
+        self.qtde += qtde
 
     def remover_qtde(self, qtde):
+        validar_numero(qtde, "Quantidade", tipo=int, permitir_zero=False)
+
+        if self.qtde - qtde < 0:
+            raise ValueError(f"Quantidade não pode ser negativa")
+
         self.qtde -= qtde #validar: restante >=0
 
     def calcular_subtotal(self):
@@ -14,8 +32,17 @@ class itemCarrinho:
 
     #métodos especiais
     def __str__(self):
-        return f'{self.produto.nome}, {self.qtde}'
+        return (f'Produto: {self.produto.nome}, '
+                f'Preço unitário: {self.produto.preco}, '
+                f'Quantidade: {self.qtde}, '
+                f'Subtotal: {self.calcular_subtotal()}'
+        )
 
-    def __eq__(self):
-        pass #verificar se o produto já está no carrinho e apenas aumentar sua quantidade
+    def __repr__(self):
+        return (f"ItemCarrinho(produto={self.produto!r}, qtde={self.qtde!r})")
 
+    def __eq__(self, outro):
+        if not isinstance(outro, ItemCarrinho):
+            return NotImplemented
+
+        return self.produto.sku == outro.produto.sku
